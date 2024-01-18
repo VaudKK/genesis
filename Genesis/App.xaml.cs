037b846bcc2045bd3df57feb16e21fb7e3c4592b
Vaud Keith <vkagong@safaricom.co.ke>
2024-01-18 23:08:49 +0300

    initial commit

---
+++ Genesis/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Genesis
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
